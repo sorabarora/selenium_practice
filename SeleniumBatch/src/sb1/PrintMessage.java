b6a20a1134dae4beca88b3fd29d46398ec482b2f
manu <manu@DESKTOP-FGJUHHM>
2023-12-04 17:45:01 +0530

    Push the code

---
+++ SeleniumBatch/src/sb1/PrintMessage.java
@@ -0,0 +1,17 @@
+package sb1;
+
+public class PrintMessage {
+	
+	public PrintMessage()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PrintMessage pm1=new PrintMessage();
+
+	}
+
+}
