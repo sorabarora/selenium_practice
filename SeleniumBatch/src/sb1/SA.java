088f38c9ce9eb13270eec2747713ff892132003a
manu <manu@DESKTOP-FGJUHHM>
2023-12-04 18:00:48 +0530

    Fourth Commit Done

--- SeleniumBatch/src/sb1/SA.java
+++ SeleniumBatch/src/sb1/SA.java
@@ -1,5 +1,15 @@
 package sb1;
 
 public class SA {
+	
+	public void print_message()
+	{
+		System.out.println("This is the message");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
