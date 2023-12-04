f3b123322aa5dd16a523ae1ab368206b92dcbba9
manu <manu@DESKTOP-FGJUHHM>
2023-12-04 18:46:57 +0530

    Final commit done

---
+++ SeleniumBatch/src/sb1/SA_1.java
@@ -0,0 +1,15 @@
+package sb1;
+
+public class SA_1 {
+	
+	public void print_message() 
+	{
+		System.out.println("Hello, Welcome To Ludhiana");
+	}
+	
+	public static void main(String[] args) {
+		
+		SA_1 sa1=new SA_1();
+	}
+
+}
